7d99a8fc3a5169e62dd4a8935cbe8c5bb004dbe7
reidkolaczek <mrkolaczek@crimson.ua.edu>
2018-04-01 22:47:27 -0500

    Revert "twitter working"
    This reverts commit 46d1c4d004ca8370692b0345c5ec7ae176bf15bc.

--- src/gathering/Twitter/utils.py
+++
@@ -1,7 +0,0 @@
-def txt2words(inputString):
-    from re import findall
-    return findall(r'(?u)\w+', inputString)
-
-def w2v_load_model(fname):
-    from gensim.models.keyedvectors import KeyedVectors
-    return KeyedVectors.load_word2vec_format(fname, binary=True)

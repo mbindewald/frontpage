6ae1a76fe4fb40b72033cf9ef8beee112d2c1909
justinjlukas1 <jjlukas@crimson.ua.edu>
2018-04-15 17:01:23 -0500

    added conversion to json

--- src/gathering/GoogleNews/googlePull.py
+++ src/gathering/GoogleNews/googlePull.py
@@ -1,3 +1,4 @@
+import json
 from newsapi import NewsApiClient
 
 
@@ -6,3 +7,5 @@ api = NewsApiClient(api_key='386530c989c5478888d26532466fdf30')
 api.get_top_headlines(sources='bbc-news')
 
 print(api.get_top_headlines(country='us'))
+with open('data.json', 'w') as outfile:
+    json.dump(api.get_top_headlines(country='us'), outfile)

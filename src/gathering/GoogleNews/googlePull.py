33aab5f8ed0f1cb4684502a5c4c66af9329c679a
justinjlukas1 <jjlukas@crimson.ua.edu>
2018-04-17 14:03:00 -0500

    no idea what these are

--- src/gathering/GoogleNews/googlePull.py
+++ src/gathering/GoogleNews/googlePull.py
@@ -4,9 +4,7 @@ from newsapi import NewsApiClient
 
 api = NewsApiClient(api_key='386530c989c5478888d26532466fdf30')
 
-api.get_top_headlines(sources='bbc-news')
-
-# print(api.get_top_headlines(q, 'specific headline', country='us', page_size=1))
+# print(api.get_top_headlines(q='specific headline', country='us', page_size=1))
 
 with open('data.json', 'w') as outfile:
     json.dump(api.get_top_headlines(country='us', page_size=1), outfile)

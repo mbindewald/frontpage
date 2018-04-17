10ad15563bad3bea2adbe333c71eeb7f26967b03
mbindewald <matthewbindewald@gmail.com>
2018-04-16 20:50:46 -0500

    API basic functionality done

--- src/api/app/main.py
+++ src/api/app/main.py
@@ -4,67 +4,36 @@ from botocore.exceptions import ClientError
 
 app = Flask(__name__)
 
-# todo: secure with auth0? So that only the right website can access api
-
+# TODO: secure with auth0? So that only the right website can access api
 # https://auth0.com/blog/developing-restful-apis-with-python-and-flask/
 
-# todo: Example trend data, will eventually be replaced with a function that grabs
-# the information from the database
-
-client = boto3.client('dynamodb')
-
+dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+table = dynamodb.Table('InformationDB')
 
 def getTrends():
     try:
-        response = client.query(
-            TableName='InformationDB',
-            IndexName='fill',
+        response = table.query(
+            KeyConditionExpression='filler = :fill',
+            ExpressionAttributeValues= {
+                ':fill': 'fill'
+            },
+            ScanIndexForward=False,
+            Limit=1
         )
-
-
     except ClientError as e:
         print(e.response['Error']['Message'])
+        return 'server error'
     else:
-        item = response['Item']
-
-    '''
-    trends =
-        {
-            "filler": "fill",
-            "time": "2008-03-09 16:05:07.123",
-            "trends": [
-                {
-                    "googleNewsEmbedLink": "xxxx.com",
-                    "trendName": "firstExample",
-                    "tweetCount": 100,
-                    "tweetEmbedLink": "xxxx.com",
-                    "youtubeEmbedLink": "xxxx.com"
-                },
-                {
-                    "googleNewsEmbedLink": "xxxx.com",
-                    "trendName": "secondExample",
-                    "tweetCount": 90,
-                    "tweetEmbedLink": "xxxx.com",
-                    "youtubeEmbedLink": "xxxx.com"
-                },
-                {
-                    "googleNewsEmbedLink": "xxxx.com",
-                    "trendName": "thirdExample",
-                    "tweetCount": 90,
-                    "tweetEmbedLink": "xxxx.com",
-                    "youtubeEmbedLink": "xxxx.com"
-                }
-            ]
-        }
-        '''
-
-    return trends
+        items = response['Items'][0]
+        del items['filler'] # remove unnecessary primary key
+        print(items)
+        return items
 
 
 @app.route("/")
 def main():
-    # Have this route be an informational page so that people hitting our
-    # server externally will know what it is.
+    # TODO: Have this route be an informational page so that people hitting our
+    # server will know what it is/have instructions to use it
 
     information = "This API is used for our CS495 project found here:"
     return information
@@ -72,6 +41,6 @@ def main():
 
 @app.route("/api")
 def api():
-    # memcache eventually for caching?
+    # TODO: memcache or similar for caching?
 
     return jsonify(getTrends())

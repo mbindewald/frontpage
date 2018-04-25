918b17a084dd73835e26abefb5747bd893e96c70
mbindewald <matthewbindewald@gmail.com>
2018-04-24 20:38:12 -0500

    fix startup script to run on correct ip and port

--- src/api/startapi.sh
+++ src/api/startapi.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 export FLASK_APP=./app/main.py
 source $(pipenv --venv)/bin/activate
-gunicorn -b localhost:5000 -w 4 --chdir ./app main:app
+gunicorn -b :5000 -w 4 --chdir ./app main:app
 flask run -h 0.0.0.0

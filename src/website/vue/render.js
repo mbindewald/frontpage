0e8126de71fe4be804e8be7c115fa2fca089526d
justinjlukas1 <jjlukas@crimson.ua.edu>
2018-04-25 17:19:05 -0500

    code for google news and sorting

--- src/website/vue/render.js
+++ src/website/vue/render.js
@@ -11,13 +11,30 @@ new Vue({
     // GET /someUrl
     this.$http.get('http://ec2-54-84-86-55.compute-1.amazonaws.com:5000/api').then(response => {
       // success callback
-      // this.results = JSON.parse(JSON.parse(response.body))
       this.results = response.body
       // this.results = atob(response.body)
       console.log("success")
+      /*
+      this.results.trends.sort(function(a, b){
+          if(a.tweet_volume < b.tweet_volume) return -1;
+          if(a.tweet_volume > b.tweet_volume) return 1;
+          return 0;
+      })
+      */
     }, response => {
       // error callback
       console.log("error")
     });
+  },
+  methods:{
+    myFunctionTweet: function (tweet) {
+        return atob(tweet.html)
+		    // this.html = atob(this.html)
+    },
+
+    myFunctionYoutube: function (youtube) {
+        return atob(youtube)
+    }
+
   }
-})
+});

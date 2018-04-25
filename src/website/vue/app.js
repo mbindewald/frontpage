fe921376ca56f1b18dbe6f5e77bca713c4f3f423
justinjlukas1 <jjlukas@crimson.ua.edu>
2018-04-24 19:36:20 -0500

    shit broke

--- src/website/vue/app.js
+++ src/website/vue/app.js
@@ -1,13 +1,39 @@
 // ./app.js
 
+new Vue({
+  el: '#app',
+  data () {
+    return {
+      info: null
+    }
+  },
+  mounted () {
+    // GET /someUrl
+    this.$http.get('http://ec2-54-84-86-55.compute-1.amazonaws.com:5000/api').then(response => {
+      // success callback
+      this.info = response.body
+      console.log("success")
+    }, response => {
+      // error callback
+      console.log(JSON.stringify(response))
+    });
+  }
+})
+
+/*
 const vm = new Vue({
   el: '#app',
   data: {
-    results: []
+    info: []
   },
   mounted() {
     axios.get('http://ec2-54-84-86-55.compute-1.amazonaws.com:5000/api').then(response => {
-      this.results = response.data.results
-    })
+      this.info = response.data
+    }, response => {
+      // error callback
+      console.log(JSON.stringify(response))
+    });
   }
+
 });
+*/

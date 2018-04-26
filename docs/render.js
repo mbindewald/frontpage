c4f53c67a060a4209b7ec8a30f5203701a4a0061
mbindewald <matthewbindewald@gmail.com>
2018-04-25 21:01:20 -0500

    move website

---
+++ docs/render.js
@@ -0,0 +1,46 @@
+// ./render.js
+
+function b64DecodeUnicode(str) {
+  return decodeURIComponent(Array.prototype.map.call(atob(str), function(c) {
+      return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2)
+  }).join(''))
+}
+
+new Vue({
+  el: '#render',
+  data () {
+    return {
+      results: null
+    }
+  },
+  mounted () {
+    // GET /someUrl
+    this.$http.get('http://ec2-54-84-86-55.compute-1.amazonaws.com:5000/api').then(response => {
+      // success callback
+      this.results = response.body
+      // this.results = atob(response.body)
+      console.log("success")
+      
+      this.results.trends.sort(function(a, b){
+          if(a.tweet_volume > b.tweet_volume) return -1;
+          if(a.tweet_volume < b.tweet_volume) return 1;
+          return 0;
+      })
+      
+    }, response => {
+      // error callback
+      console.log("error")
+    });
+  },
+  methods:{
+    myFunctionTweet: function (tweet) {
+        return b64DecodeUnicode(tweet.html)
+		    // this.html = atob(this.html)
+    },
+
+    myFunctionYoutube: function (youtube) {
+        return b64DecodeUnicode(youtube)
+    }
+
+  }
+});
